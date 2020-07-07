52c2910b6136e7483e76dd0dbe65136bdc99d97a
Joamir Silveira <jmrsilveira@outlook.com>
2020-07-07 10:31:15 -0300

    Creating the builder

--- account.py
+++ account.py
@@ -1,3 +1,8 @@
 
 class Account:
-    pass
+    def __init__(self, number_account, cardholder_name, balance_account, limit_account=1000):
+        print(f'Criando o objeto {self}')
+        self.number_account = number_account
+        self.cardholder_name = cardholder_name
+        self.balance_account = balance_account
+        self.limit_account = limit_account
