7388b99b1f62866ff8e312fd89400e3debbb3006
Joamir Silveira <jmrsilveira@outlook.com>
2020-07-13 10:58:45 -0300

    Encapsulating attributes with two underscore

--- account.py
+++ account.py
@@ -2,16 +2,16 @@
 class Account:
     def __init__(self, number_account, cardholder_name, balance_account, limit_account=1000):
         print(f'Criando o objeto {self}')
-        self.number_account = number_account
-        self.cardholder_name = cardholder_name
-        self.balance_account = balance_account
-        self.limit_account = limit_account
+        self.__number_account = number_account
+        self.__cardholder_name = cardholder_name
+        self.__balance_account = balance_account
+        self.__limit_account = limit_account
 
     def account_statement(self):
-        print(f"Saldo {self.balance_account} do titular {self.cardholder_name}")
+        print(f"Saldo {self.__balance_account} do titular {self.__cardholder_name}")
 
     def deposit_money(self, value):
-        self.balance_account += value
+        self.__balance_account += value
 
     def withdraw_money(self, value):
-        self.balance_account -= value
+        self.__balance_account -= value
