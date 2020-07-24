ff0a8bbfc4225aa80a06148f8f0b725ce4cdfc17
Joamir Silveira <jmrsilveira@outlook.com>
2020-07-24 11:03:09 -0300

    Creating mother class

--- Orientado-objeto-2/model.py
+++ Orientado-objeto-2/model.py
@@ -1,16 +1,15 @@
-class Movie:
-    def __init__(self, name, year, duration):
+class Program:
+    def __init__(self, name, year):
         self.__name = name.title()
         self.year = year
-        self.duration = duration
-        self.__like = 0
+        self.__likes = 0
 
     @property
-    def like(self):
-        return self.__like
+    def likes(self):
+        return self.__likes
 
     def give_likes(self):
-        self.__like += 1
+        self.__likes += 1
 
     @property
     def name(self):
@@ -18,34 +17,27 @@ class Movie:
 
     @name.setter
     def name(self, new_name):
-        self.__name = new_name
+        self.__name = new_name.title()
 
 
-class Series:
+class Movie(Program):
+    def __init__(self, name, year, duration):
+        self.__name = name.title()
+        self.year = year
+        self.duration = duration
+        self.__like = 0
+
+
+class Series(Program):
     def __init__(self, name, year, season):
         self.name = name.title()
         self.year = year
         self.season = season
         self.__like = 0
 
-    @property
-    def like(self):
-        return self.__like
 
-    def give_likes(self):
-        self.__like += 1
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, name):
-        self.__name = name
-
-
-avengers = Movie('Avengers', 2012, 160)
-friends = Series('Friends', 1994, 10)
+avengers = Movie('avengers', 2012, 160)
+friends = Series('friends', 1994, 10)
 
 print(f'Filme: {avengers.name}, Ano: {avengers.year}, Duração: {avengers.duration}')
 print(f'Série: {friends.name}, Ano: {friends.year}, Temporadas: {friends.season}')
