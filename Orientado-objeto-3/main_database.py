aef45914aaa47b90b9af00f2e57b5324fd8a9cd1
JoamirS <jmrsilveira@outlook.com>
2021-05-19 20:44:06 -0300

    Leaving the variable private

--- Orientado-objeto-3/main_database.py
+++ Orientado-objeto-3/main_database.py
@@ -5,20 +5,20 @@ public, protected, private
 
 class Database:
     def __init__(self):
-        self.data = {}
+        self._data = {}
 
     def insert_client(self, id_client, name_client):
-        if 'clientes' not in self.data:
-            self.data['clientes'] = {id_client: name_client}
+        if 'clientes' not in self._data:
+            self._data['clientes'] = {id_client: name_client}
         else:
-            self.data['clientes'].update({id_client: name_client})
+            self._data['clientes'].update({id_client: name_client})
 
     def list_customers(self):
-        for id_client_dict, name_client_dict in self.data['clientes'].items():
+        for id_client_dict, name_client_dict in self._data['clientes'].items():
             print(id_client_dict, name_client_dict)
 
     def delete_client(self, id_client):
-        del self.data['clientes'][id_client]
+        del self._data['clientes'][id_client]
 
 
 database_test = Database()
@@ -27,4 +27,4 @@ database_test.insert_client(2, 'Moraes')
 database_test.insert_client(3, 'Silveira')
 database_test.list_customers()
 
-print(database_test.data)
+print(database_test._data)
