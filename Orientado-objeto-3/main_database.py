2ff948186255c858ebfc54ead62164264bda99e6
JoamirS <jmrsilveira@outlook.com>
2021-05-19 23:15:04 -0300

    Create get (@Property)

--- Orientado-objeto-3/main_database.py
+++ Orientado-objeto-3/main_database.py
@@ -5,20 +5,24 @@ public, protected, private
 
 class Database:
     def __init__(self):
-        self._data = {}
+        self.__data = {}
+
+    @property
+    def get_data(self):
+        return self.__data
 
     def insert_client(self, id_client, name_client):
-        if 'clientes' not in self._data:
-            self._data['clientes'] = {id_client: name_client}
+        if 'clientes' not in self.__data:
+            self.__data['clientes'] = {id_client: name_client}
         else:
-            self._data['clientes'].update({id_client: name_client})
+            self.__data['clientes'].update({id_client: name_client})
 
     def list_customers(self):
-        for id_client_dict, name_client_dict in self._data['clientes'].items():
+        for id_client_dict, name_client_dict in self.__data['clientes'].items():
             print(id_client_dict, name_client_dict)
 
     def delete_client(self, id_client):
-        del self._data['clientes'][id_client]
+        del self.__data['clientes'][id_client]
 
 
 database_test = Database()
@@ -26,5 +30,6 @@ database_test.insert_client(1, 'Joamir')
 database_test.insert_client(2, 'Moraes')
 database_test.insert_client(3, 'Silveira')
 database_test.list_customers()
+print(database_test.get_data)
 
-print(database_test._data)
+
